a8a9259ab5d8a934cc79042482cd1663d4bfd90e
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2026-06-05 23:12:16 +0100

    Created all the routing in the app jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,31 @@
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Background from "./components/Background/Background";
 import Logo from "./components/Logo";
 import Menu from "./components/Menu";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Projects from "./pages/Projects";
+import Experience from "./pages/Experience";
 import Contact from "./pages/Contact";
 import "./App.css";
 
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
         <Logo />
-        <Menu />
+        <Menu opened={menuOpen} setOpened={setMenuOpen} />
         <Background />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
+        {!menuOpen && <Footer />}
       </header>
     </div>
   );
